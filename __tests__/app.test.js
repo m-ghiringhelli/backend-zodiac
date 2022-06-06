913b3a3b05df4f07725632e5f52309b877ed295d
m-ghiringhelli <mghiring@gmail.com>
2022-06-06 16:29:10 -0700

    test for returning zodiac signs

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,11 +3,19 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-const { }
+const { zodiac } = require('../data/zodiac.js');
 
 describe('backend-zodiac routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('/zodiac should return zodiac signs', async () => {
+    const response = await request(app).get('/zodiac');
+    const expected = zodiac.map((sign) => {
+      return { id: sign.id, name: sign.name };
+    });
+    expect(response.body).toEqual(expected);
   });
 
   afterAll(() => {
